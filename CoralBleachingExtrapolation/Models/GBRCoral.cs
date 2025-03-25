52606ab7e036dc24daf402ce1a8a1e4ccafeb0e1
Hin Chi Wong <h_wong239240@fanshaweonline.ca>
2025-03-25 17:31:24 -0400

    hi

--- CoralBleachingExtrapolation/Models/GBRCoral.cs
+++ CoralBleachingExtrapolation/Models/GBRCoral.cs
@@ -5,11 +5,14 @@ using NetTopologySuite.Algorithm;
 using CoralBleachingExtrapolation.Data;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 /// <summary>
 /// 03-02-2025  1.0     Keelin   Point GBR Implimenting UML (GBRCoral.cs) 
 /// 03-02-2025  1.0.1   Keelin   Fixing references and errors (InvalidOperationException, InvalidColumnName) 
 /// 03-22-2025  1.0.2   Keelin   Making it work with the index 
+/// 03-24-2025  1.1     Ben      Json Serialize
 /// </summary>
 /// 
 
@@ -51,9 +54,19 @@ namespace CoralBleachingExtrapolation.Models
         public double? MeanLiveCoral { get; set; }
         public double? MeanSoftCoral { get; set; }
         public double? MeanDeadCoral { get; set; }
-        public int?  MedianLiveCoralFK { get; set; }
+        public int? MedianLiveCoralFK { get; set; }
         public int? MedianSoftCoralFK { get; set; }
-        public  int? MedianDeadCoralFK { get; set; }
-    }
+        public int? MedianDeadCoralFK { get; set; }
 
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(new
+            {
+                Latitude = Latitude.HasValue ? (float)Latitude.Value : (float?)null,
+                Longitude = Longitude.HasValue ? (float)Longitude.Value : (float?)null,
+                ReefName
+            });
+        }
+    }
 }
+
