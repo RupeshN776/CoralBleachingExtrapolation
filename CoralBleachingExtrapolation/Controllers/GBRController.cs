8d7ae86b2145032c304d792b3b8e0f776113a92b
Rupesh N <rupeshnikumbhe65@gmail.com>
2025-03-31 09:48:26 -0400

    error handlingin

--- CoralBleachingExtrapolation/Controllers/GBRController.cs
+++ CoralBleachingExtrapolation/Controllers/GBRController.cs
@@ -16,6 +16,7 @@ using System.Text.Json;
 /// 03-15-2025   1.0.1       Keelin     bug fixes
 /// 03-22-2025   1.0.2       Keelin     more bug fixes
 /// 03-22-2025   PROMOTE     Rupesh     PROMOTE TESTING and COMMENTS
+/// 03-24-2025   1.0.2       Keelin     bug fixes based on promote code
 /// </summary>
 /// 
 
@@ -40,7 +41,8 @@ namespace CoralBleachingExtrapolation.Controllers
             //TheModel = _db.tbl_GlobalCoralPolygon.ToList();
 
             TheModel = _db.tbl_GBRCoralPoint
-                               .OrderByDescending(x => x.GBRCoralPointID) // Sort by ID in descending                   // Get the last 10 records
+                               .OrderByDescending(x => x.GBRCoralPointID) // Sort by ID in descending order
+                               .Take(10)                     // Get the last 10 records
                                .ToList();
 
 
@@ -48,7 +50,7 @@ namespace CoralBleachingExtrapolation.Controllers
             return View(TheModel);
         }
 
-        //--------------------------------Read One:TODO----------------------------------//
+        //--------------------------------Read One:TODO----------------------------------// //PROMOTE: mising try catch why? // only implemented on assembly, does not interact with client. 
         public IActionResult Read(int id)
         {
             if (id == null || id == 0)
@@ -74,12 +76,22 @@ namespace CoralBleachingExtrapolation.Controllers
 
         [HttpPost]
 
-        public IActionResult Create(GBRCoralPoint obj)  //does not check report year input. //default lat lng are antartica
+        public IActionResult Create(GBRCoralPoint obj)  //PROMOTE: does not check report year input. default lat lng are antartica does not ask for client input. 
         {
+            if (obj.ReportYear < 1980 || obj.ReportYear > 2025)
+            {
+                ModelState.AddModelError("ReportYear", "Report Year must be between 1980 and 2025.");
+            }
 
-            obj.Latitude = 1;
+            obj.Latitude = 1; //default initial
             obj.Longitude = 1;
 
+            //string wkt = $"POINT ({obj.Longitude} {obj.Latitude})"; // WKT for a single point //PROMOTE: why in antartica? //fixed
+
+            if (!ModelState.IsValid)
+            {
+                return View(obj); // Return to form
+            }
 
             string wkt = "POINT (-75.0 40.0)"; // WKT for a single point //why in antartica
 
@@ -92,9 +104,10 @@ namespace CoralBleachingExtrapolation.Controllers
                 if (geometry is Point point)
                 {
                     obj.Point = point;
-                    obj.Point.SRID = 4326; 
+                    obj.Point.SRID = 4326;
                     _db.tbl_GBRCoralPoint.Add(obj);
                     _db.SaveChanges();
+                    return RedirectToAction("Index");
                 }
                 else
                 {
@@ -102,7 +115,7 @@ namespace CoralBleachingExtrapolation.Controllers
                     return View("Error");
                 }
 
-                return View();
+                return View(obj);
             }
             catch (NetTopologySuite.IO.ParseException ex)
             {
@@ -119,12 +132,12 @@ namespace CoralBleachingExtrapolation.Controllers
 
 
         //--------------------------------Update----------------------------------//
-        //GBR Update - does not test input year, WKT format does not reflect updated Lat Long.
+        //GBR Update - does not test input year, WKT format does not reflect updated Lat Long. //sprint 5 assigned. 
         //GBR update - breaks upon invalid lat lng input
 
 
-                [HttpGet]
-        public IActionResult Update(int? id) // Update ??PROMTE NOTE: purpose for overload? name differently perhaps? 
+        [HttpGet]
+        public IActionResult Update(int? id) // Update ??PROMTE NOTE: purpose for overload? name differently perhaps? //only used for button interaction on client side. //okay
         {
             if (id == null || id == 0)
             {
@@ -141,15 +154,36 @@ namespace CoralBleachingExtrapolation.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(GBRCoralPoint obj, string ShapeWkt) 
+        public IActionResult Update(GBRCoralPoint obj, string ShapeWkt)  //PROMOTE: CHECK LAT LONG INPUT AND REPORT YEAR. //fixed //P: WKT DOES NOT REFLECT LAT LNG CHANGES. //agreed to do next sprint if not possible now. 
         {
+            // check lat
+            if (obj.Latitude < -90 || obj.Latitude > 90)
+            {
+                ModelState.AddModelError("Latitude", "Invalid Input. Latitude must be between -90 and 90."); //check lat
+            }
+            //check long
+            if (obj.Longitude < -180 || obj.Longitude > 180)
+            {
+                ModelState.AddModelError("Longitude", "Invalid Input. Longitude must be between -180 and 180."); //check long
+            }
+
+            if (obj.ReportYear < 1980 || obj.ReportYear > 2025)
+            {
+                ModelState.AddModelError("ReportYear", "Report Year must be between 1980 and 2025."); //check report year
+            }
+
             if (!string.IsNullOrEmpty(ShapeWkt))
             {
                 try
                 {
                     var geometryFactory = new GeometryFactory(new PrecisionModel(), 4326); // SRID 4326 for geography
                     var reader = new WKTReader(geometryFactory);
+
                     obj.Point = (Point)reader.Read(ShapeWkt); // Convert WKT to Polygon
+
+                    string newWkt = $"POINT ({obj.Longitude} {obj.Latitude})"; //refelct the updated lat long. 
+
+                    obj.Point = (Point)reader.Read(newWkt);
                     obj.Point.SRID = 4326;
                 }
                 catch (Exception ex)
@@ -161,14 +195,14 @@ namespace CoralBleachingExtrapolation.Controllers
             if (ModelState.IsValid)
             {
                 _db.tbl_GBRCoralPoint.Update(obj);
-                _db.SaveChanges(); //error when lat or long input is invalid CHECK
+                _db.SaveChanges(); //P: error when lat or long input is invalid CHECK
                 return RedirectToAction("Index");
             }
 
-            return View(obj);
+            return View(obj); //keeps the loop if they input incorrect values. 
         }
         //--------------------------------Delete:TODO----------------------------------// 
-        //DID NOT TEST - non-functional
+        //PROMOTE: DID NOT TEST - non-functional
 
         public IActionResult Delete(GBRCoralPoint obj) //Delete
         {
--- CoralBleachingExtrapolation/Controllers/HomeController.cs
+++ CoralBleachingExtrapolation/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ using System.Text.RegularExpressions;
 /// <summary>
 /// Date        Version          Name        Comment
 /// 25-2-2025   1.0              Ben         Create, Read, Update, Delete template
-/// 24-3-2025   1.0              Ben         I go to school by bus
+/// 03-22-2025  PROMOTE          Rupesh      PROMOTE TESTING and COMMENTS
+/// 27-3-2025   1.1              Ben         Bug fixes based on promote comments
 /// </summary>
 
 
@@ -29,6 +30,8 @@ public class HomeController : Controller
         _db = db;
     }
 
+    //PROMOTE: COMMENT ALL
+
     //--------------------------------Read All----------------------------------//
     public IActionResult Index() // Read
     {
@@ -42,46 +45,49 @@ public class HomeController : Controller
         return View(TheModel);
     }
 
-    //--------------------------------Read One:TODO----------------------------------//
+    //--------------------------------Read One:TODO----------------------------------// PROMOTE: MISSING TRY CATACH WHY? //only used in assembly
     public IActionResult Read(int id)
     {
-        if (id == null || id == 0)
+        if (id == null || id == 0) //check if valid ID
         {
             return NotFound();
         }
 
-        WorldCoral? Coral = _db.tbl_GlobalCoralPolygon.Find(id);
+        WorldCoral? Coral = _db.tbl_GlobalCoralPolygon.Find(id); //retrieve coral object
 
-        if (Coral == null)
+        if (Coral == null) //check null coral object
         {
             return NotFound();
         }
-        return View(Coral);
+        return View(Coral);//return
     }
 
 
     //--------------------------------Create----------------------------------//
     public IActionResult Create() //Create
     {
-        return View();  
+        return View();
     }
 
     [HttpPost]
-    public IActionResult Create(WorldCoral obj) //Create
+    public IActionResult Create(WorldCoral obj) //Create PROMOTE: TRY CATCH THIS BRO
     {
-            //test only
-            obj.RegionFK = 1;
-            obj.GISAREAKM2 = 1;
-            string wkt = "POLYGON((-75.0 40.0, -73.0 40.5, -74.0 41.0, -75.0 40.0))";
-            var geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
-            var wktReader = new WKTReader(geometryFactory);
-            Polygon polygon = (Polygon)wktReader.Read(wkt);
-            obj.Shape = polygon;
-            obj.Shape.SRID = 4326;
-            //test only
-            _db.tbl_GlobalCoralPolygon.Add(obj);
-            _db.SaveChanges();
-            return View();
+        //test only -promote: finalized this as we need to create standardized ones next sprint. 
+        //finalized - standard polygons in progress. 
+        //P: okay
+
+        obj.RegionFK = 1;
+        obj.GISAREAKM2 = 1;
+        string wkt = "POLYGON((-75.0 40.0, -73.0 40.5, -74.0 41.0, -75.0 40.0))";
+        var geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+        var wktReader = new WKTReader(geometryFactory);
+        Polygon polygon = (Polygon)wktReader.Read(wkt);
+        obj.Shape = polygon;
+        obj.Shape.SRID = 4326;
+        //test only
+        _db.tbl_GlobalCoralPolygon.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
 
 
@@ -91,21 +97,28 @@ public class HomeController : Controller
     {
         if (id == null || id == 0)
         {
-            return NotFound();
+            return NotFound(); //check valid id
         }
 
         WorldCoral? Coral = _db.tbl_GlobalCoralPolygon.Find(id);
 
         if (Coral == null)
         {
-            return NotFound();
+            return NotFound(); //check coral object
         }
         return View(Coral);
     }
 
     [HttpPost]
-    public IActionResult Update(WorldCoral obj, string ShapeWkt)
+    public IActionResult Update(WorldCoral obj, string ShapeWkt) ///PROMOTE: DOES NOT CHECK AREA INPUT //fixed
     {
+        if (obj.GISAREAKM2 < 0 || obj.GISAREAKM2 > 9858.92)
+        {
+            ModelState.AddModelError("GISAREAKM2", "Invalid Input."); //control the area input for a polygon
+
+        }
+
+
         if (!string.IsNullOrEmpty(ShapeWkt))
         {
             try
@@ -130,7 +143,7 @@ public class HomeController : Controller
 
         return View(obj);
     }
-    //--------------------------------Delete:TODO----------------------------------//
+    //--------------------------------Delete:TODO----------------------------------// PROMOTE: THROWS ERROR, REMOVE FROM CLIENT SIDE || FIX
 
     public IActionResult Delete(WorldCoral obj) //Delete
     {
@@ -145,6 +158,8 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    //PROMOTE: WORK TOWARDS MAP INSERT
 
     //--------------------------------TODO: CORAL POLYGON VIEW FOR MAP----------------------------------//
 
